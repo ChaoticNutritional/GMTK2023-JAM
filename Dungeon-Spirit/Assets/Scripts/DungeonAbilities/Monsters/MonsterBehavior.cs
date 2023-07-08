bcc37cc281fe3a2e206187c4184fc53799066e23
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-07-08 13:13:43 -0400

    File Restructuring

---
+++ Dungeon-Spirit/Assets/Scripts/DungeonAbilities/Monsters/MonsterBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Have health
+
+    // Have Dmg
+
+    // Wait for hero to enter tile
+
+    // When hero enter tile, wait for hero to attack
+        // attack
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
