185e4c9cbec177ab1460f1eb7f2b052dcb95f9b2
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-07-07 21:52:00 -0400

    Character Controller in a normal place

--- Dungeon-Spirit/Assets/Scripts/CameraController.cs
+++ Dungeon-Spirit/Assets/Scripts/CameraController.cs
@@ -8,12 +8,9 @@ using UnityEditor.Events;
 
 public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
 {
-    private float m_TargetPitch = 45f;
-    private int m_CurrentZoomLevel;
     private CameraControls camControls;
     private InputAction movement;
     public Transform cameraTransform;
-    private Vector3 m_NewZoom;
     public Vector2 MovementValue { get; private set; }
     public Vector2 RotateValue { get; private set; }
     public float ZoomValue { get; private set; }
@@ -25,44 +22,25 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
 
     // Variables for camera controls
     // panning motion
-    [SerializeField] private float _panMoveSpeed;
-    [SerializeField] private static float _panTopSpeed = 5f;
-    [SerializeField] private float _panAcceleration = 10f;
+    [SerializeField] private float panMoveSpeed;
+    [SerializeField] private static float panTopSpeed = 5f;
+    [SerializeField] private float panAcceleration = 10f;
     [SerializeField] private float damping = 10f;
 
     // zoom motion
-    [SerializeField] private float _zoomSpeed;
-    [SerializeField] private float _stepSize;
-    [SerializeField] private float _zoomDamping;
-    [SerializeField] private float _minHeight = 5f;
-    [SerializeField] private float _maxHeight = 50f;
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float stepSize;
+    [SerializeField] private float zoomDamping;
+    [SerializeField] private float minHeight = 5f;
+    [SerializeField] private float maxHeight = 50f;
 
     // rotation
-    [SerializeField] private float _rotationTopSpeed = 3f;
-    [SerializeField] private float _rotationSpeed;
-    [SerializeField] private float _rotationAcceleration = 5f;
+    [SerializeField] private float rotationTopSpeed = 3f;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float rotationAcceleration = 5f;
 
     // screen edge motion
-    [SerializeField] private bool _useScreenEdge = true;
-
-    private enum ZoomLevelDirection
-    {
-        Up,
-        Down
-    }
-
-    [SerializeField]
-    [Tooltip("Tells the camera which direction the Zoom Level percentages go. Up means that the first index is fully zoomed in (0), and ascends from there." +
-         " Down means that the first index is fully zoomed out (1), and descends from there.")]
-    private ZoomLevelDirection m_ZoomLevelDirection = ZoomLevelDirection.Down;
-    [SerializeField]
-    [Tooltip("If true, when zooming in and out, the camera will zoom to the next or previous level defined in Zoom Levels")]
-    private bool m_UseZoomLevels = false;
-
-    [SerializeField]
-    [Tooltip("List of zoom levels to use (as percentages between 0 and 1) with 1 being all the way zoomed out and 0 being all the way zoomed in." +
-             " These are only used if Use Zoom Levels is true")]
-    private List<float> m_ZoomLevels = null;
+    [SerializeField] private bool useScreenEdge = true;
 
     // Where we're trying to move TO
     // We will be lerping towards that position
@@ -96,7 +74,7 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
     {
         if (!UnlockRotation) return;
         float rotationValue = context.ReadValue<Vector2>().x;
-        transform.rotation = Quaternion.Euler(0f, rotationValue * _rotationSpeed + transform.rotation.eulerAngles.y, 0f);
+        transform.rotation = Quaternion.Euler(0f, rotationValue * rotationSpeed + transform.rotation.eulerAngles.y, 0f);
     }
 
     public void OnSelect(InputAction.CallbackContext context)
@@ -122,7 +100,6 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
 
     private void Awake()
     {
-        m_NewZoom = cameraTransform.localPosition;
         camControls = new CameraControls();
     }
 
@@ -141,14 +118,14 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
 
         if (Mathf.Abs(value) > 0.1f)
         {
-            zoomHeight = cameraTransform.localPosition.y + value * _stepSize;
-            if (zoomHeight < _minHeight)
+            zoomHeight = cameraTransform.localPosition.y + value * stepSize;
+            if (zoomHeight < minHeight)
             {
-                zoomHeight = _minHeight;
+                zoomHeight = minHeight;
             }
-            else if (zoomHeight > _maxHeight)
+            else if (zoomHeight > maxHeight)
             {
-                zoomHeight = _maxHeight;
+                zoomHeight = maxHeight;
             }
         }
     }
@@ -194,8 +171,8 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
         if (targetPosition.sqrMagnitude > 0.1f)
         {
             // move towards it
-            _panMoveSpeed = Mathf.Lerp(_panMoveSpeed, _panTopSpeed, Time.deltaTime * _panAcceleration);
-            transform.position += targetPosition * _panMoveSpeed * Time.deltaTime;
+            panMoveSpeed = Mathf.Lerp(panMoveSpeed, panTopSpeed, Time.deltaTime * panAcceleration);
+            transform.position += targetPosition * panMoveSpeed * Time.deltaTime;
         }
         else
         {
@@ -209,9 +186,9 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
     private void UpdateZoom()
     {
         Vector3 zoomTarget = new Vector3(cameraTransform.localPosition.x, zoomHeight, cameraTransform.localPosition.z);
-        zoomTarget -= _zoomSpeed * (zoomHeight - cameraTransform.localPosition.y) * Vector3.forward;
+        zoomTarget -= zoomSpeed * (zoomHeight - cameraTransform.localPosition.y) * Vector3.forward;
 
-        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, zoomTarget, Time.deltaTime * _zoomDamping);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, zoomTarget, Time.deltaTime * zoomDamping);
         cameraTransform.LookAt(this.transform);
     }
 
@@ -221,14 +198,14 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
 
         if (Mathf.Abs(value) > 0.1f)
         {
-            zoomHeight = cameraTransform.localPosition.y + value * _stepSize;
-            if (zoomHeight < _minHeight)
+            zoomHeight = cameraTransform.localPosition.y + value * stepSize;
+            if (zoomHeight < minHeight)
             {
-                zoomHeight = _minHeight;
+                zoomHeight = minHeight;
             }
-            else if (zoomHeight > _maxHeight)
+            else if (zoomHeight > maxHeight)
             {
-                zoomHeight = _maxHeight;
+                zoomHeight = maxHeight;
             }
         }
     }
@@ -255,9 +232,9 @@ public class CameraController : MonoBehaviour, CameraControls.IMyCamActions
     {
         if (targetPosition.sqrMagnitude > .1f)
         {
-            _panMoveSpeed = Mathf.Lerp(_panMoveSpeed, _panTopSpeed, Time.deltaTime * _panAcceleration);
-            Debug.Log("move speed = " + _panMoveSpeed);
-            transform.position += targetPosition * _panMoveSpeed * Time.deltaTime;
+            panMoveSpeed = Mathf.Lerp(panMoveSpeed, panTopSpeed, Time.deltaTime * panAcceleration);
+            Debug.Log("move speed = " + panMoveSpeed);
+            transform.position += targetPosition * panMoveSpeed * Time.deltaTime;
         }
         else
         {
