e396adc8af327a3dac3514f87b142a0c43487927
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-07-08 10:45:27 -0400

    Test Floor Highlight on mouse over

---
+++ Dungeon-Spirit/Assets/Scripts/MouseEntering.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseEntering : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    // Start is called before the first frame update
+
+    private Material myMat;
+    private Color originalColor;
+    private bool touched = false;
+
+    void Awake()
+    {
+        myMat = this.GetComponent<MeshRenderer>().material;
+        originalColor = myMat.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myMat.color = Color.green;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myMat.color = originalColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Teehee");
+    }
+}
