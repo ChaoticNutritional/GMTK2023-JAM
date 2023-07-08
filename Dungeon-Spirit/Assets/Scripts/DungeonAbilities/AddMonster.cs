524c22d798857ca145b6c2fcf8a098b2ea400f7e
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-07-08 14:25:21 -0400

    creation of template scripts

---
+++ Dungeon-Spirit/Assets/Scripts/DungeonAbilities/AddMonster.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMonster : MonoBehaviour
+{
+    // Can be used as versatile ability holder for any Monster to add
+
+    // Must have a reference to monster to add
+        // can be set in editor
+
+    // When clicked from ability bar
+    // Temp selected
+        // if hovering over ground tile
+        // if tile valid
+        // render preview of mesh with transparent version of material
+    
+    // When placed
+        // instantiate REFERENCED monster on tile hovered over.
+}
--- Dungeon-Spirit/Assets/Scripts/DungeonAbilities/Monsters/MonsterBehavior.cs
+++ Dungeon-Spirit/Assets/Scripts/DungeonAbilities/Monsters/MonsterBehavior.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class MonsterBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    // Have health
-
-    // Have Dmg
+    [SerializeField] protected float _health;
+    [SerializeField] protected float _damage;
+    [SerializeField] protected float _xpReward;
+    [SerializeField] protected GameObject _target;
 
     // Wait for hero to enter tile
+        // TODO
+        // Have a reference to a dungeon tile that it's on
+            // so that we may check if a hero entered it
+        
+        // Have that dungeonTile pass reference of the player to the monster
 
     // When hero enter tile, wait for hero to attack
         // attack
-
 
     void Start()
     {
