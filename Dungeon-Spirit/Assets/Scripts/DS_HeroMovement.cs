3ccb0f8a1e42992bc78895031dcd9322006a8db2
Marshall Nystrom <marshall.nystrom@gmail.com>
2023-07-09 08:55:54 -0700

    HeroMovement started

--- Dungeon-Spirit/Assets/Scripts/DS_HeroMovement.cs
+++ Dungeon-Spirit/Assets/Scripts/DS_HeroMovement.cs
@@ -5,26 +5,57 @@ using UnityEngine;
 public class DS_HeroMovement : MonoBehaviour
 {
     public GameObject[] testerObjects;
-    private Collider[] testerColliders;
+    private DS_TestColliderScript[] testerScripts;
     private float[] tileDesire;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 
-    public void CheckMovementDirection()
+    public Vector3 CheckMovementDirection()
     {
         for(int i = 0; i < testerObjects.Length; i++)
         {
-            if(testerColliders[i].)
+            if(testerScripts[i].isColliding)
+            {
+                tileDesire[i] -= 10000f;
+            }
+            RaycastHit hit;
+            if(Physics.Raycast(testerObjects[i].transform.position, Vector3.down, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag("FloorTile"))
+                {
+                    TileInputHandler tile = hit.collider.GetComponent<TileInputHandler>();
+                    switch (tile.tileState)
+                    {
+                        case TileInputHandler.TileState.pit:
+                            break;
+                        case TileInputHandler.TileState.floor:
+                            break;
+                        case TileInputHandler.TileState.raised:
+                            break;
+                        case TileInputHandler.TileState.enemy:
+                            break;
+                        case TileInputHandler.TileState.spike:
+                            break;
+                        case TileInputHandler.TileState.fountain:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
+
+
+        return Vector3.zero;
     }
 }
