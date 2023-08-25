1b531505f1469d96aeb605e7c090199a06ec631b
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-08-25 19:09:36 -0400

    A* Pathfinding Testing
    In the process of creating a framework to use A* graphing algorithm for the "Hero" character to work towards the end of the dungeon.

---
+++ Dungeon-Spirit/Assets/Scripts/Pathfinding/AGrid.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:16cf221d22356d5f3409af44f037dbc13e0e111fa0ec1b0fa4d90d77ed74c846
+size 2325
---
+++ Dungeon-Spirit/Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:af424af21a8dd86d498d29d248e26f897e3bbf4de6df3c428e7a925029c36ea0
+size 450
