b4c5beb9829ed1145c46cffec3f6d44610c22f43
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2023-08-25 20:08:06 -0400

    Fixed camera controller
    Last push containing camera controller script was not in good operating order

--- Dungeon-Spirit/Assets/Scripts/CameraController.cs
+++ Dungeon-Spirit/Assets/Scripts/CameraController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be13f0017289d15c13760af64f147d4afbe084976ab9800dab6490a1b6234631
-size 8219
+oid sha256:e71a365d562a6d692eced5aee70ab8fab478563010fdc62eca0df96f8fcefa04
+size 9409
